9940739ef74b107789cf624c84cef9ca849b5b62
abdul-malik <abdulmalikmohamed360@gmail.com>
2021-08-11 13:56:10 +0200

    update

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,21 @@
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-
 const authentication = "https://abdul-malik-api.herokuapp.com/auth";
 
-fetch(authentication, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ "username": username, "password": password })
-}).then((res) => res.json())
-  .then((res) => {
-    console.log(res);
-    myStorage = window.localStorage;
-    console.log(res["access_token"]);
-    myStorage.setItem("jwt-token", res["access_token"]);
-  });
+function login() {
+  fetch(authentication, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      myStorage = window.localStorage;
+      console.log(res["access_token"]);
+      myStorage.setItem("jwt-token", res["access_token"]);
+    });
+}
